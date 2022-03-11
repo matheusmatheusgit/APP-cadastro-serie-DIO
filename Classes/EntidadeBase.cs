a89da9d4b4742d2950c974e4ed752ce1f3ed43ce
matheusmatheusgit <matheusmatheus@hotmail.com>
2022-03-10 21:17:26 -0300

    APP-Cadastro-Serie

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace APP-cadastro-serie-DIO
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+
+# App Cadastro em serie .NET #
+
+
+# Projeto Desenvolvido no Bootcamp DIO ministrado por Eliézer Zarpelão #
+
+
